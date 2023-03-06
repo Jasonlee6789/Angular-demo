5f2fd625311f9e92d2a025122c20e6450c0a7aae
Jeff <li.jing@smartcompany.jp>
2023-03-06 19:59:42 +0900

    fix: begin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  hanjin() {
+    alert('lijingaiiii');
+  }
   title = 'lijing-app';
+  name = 'lijing';
 }

7f764b72762abe3e0927e2dfc7aa4e5b5e8aff5a
Jeff <li.jing@smartcompany.jp>
2023-03-08 19:09:42 +0900

    feat:  displays a list of heroes with a detail view and select a hero and see that hero's details.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss'],
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
+  // onSelectHero(_t5: Hero) {
+  //   throw new Error('Method not implemented.');
+  // }
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   // hero = 'Windstorm';
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm',

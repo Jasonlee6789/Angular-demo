aaf561939715464fd9604a5c93b8309b9469de0f
Jeff <li.jing@smartcompany.jp>
2023-03-09 09:31:55 +0900

    feat:  created a separate, reusable HeroDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LijingSecondComponent } from './a/b/c/lijing-second/lijing-second.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
 @NgModule({
   // 项目中使用组件的声明
-  declarations: [AppComponent, LijingSecondComponent, HeroesComponent],
+  declarations: [AppComponent, LijingSecondComponent, HeroesComponent, HeroDetailComponent],
   // 引用的第三方库
   imports: [BrowserModule, FormsModule],
   // 与整个 app 生命周期关联的服务（service）
